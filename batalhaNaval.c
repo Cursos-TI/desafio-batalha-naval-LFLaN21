4f25e58ac6008e140e5b9849c275e80ada351abc
LFLaN21 <riquelmefreire_@hotmail.com>
2025-05-01 21:43:47 +0000

    concluisaoDoDesafioIniciante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,52 +1,29 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-#define TAMANHO 10
 
 int main() {
-    // Tabuleiro 10x10 (0 = água, 1 = navio)
-    int tabuleiro[TAMANHO][TAMANHO] = {0};
+    int tabuleiro[10][10] = {0}; // 0 = água
     
-    // Navio 1 - horizontal (tamanho 4)
-    int navio1_x = 2;
-    int navio1_y = 3;
-    int tamanho_navio1 = 3;
+    // Navio horizontal (3 posições)
+    tabuleiro[2][3] = 3;  // Linha 3, Coluna D
+    tabuleiro[2][4] = 3;  // Linha 3, Coluna E
+    tabuleiro[2][5] = 3;  // Linha 3, Coluna F
     
-    // Navio 2 - vertical (tamanho 3)
-    int navio2_x = 5;
-    int navio2_y = 5;
-    int tamanho_navio2 = 3;
+    // Navio vertical (4 posições)
+    tabuleiro[5][7] = 3;  // Linha 6, Coluna H
+    tabuleiro[6][7] = 3;  // Linha 7, Coluna H
+    tabuleiro[7][7] = 3;  // Linha 8, Coluna H
+    tabuleiro[8][7] = 3;  // Linha 9, Coluna H
     
-    // Posicionar navio 1 (horizontal)
-    for (int i = 0; i < tamanho_navio1; i++) {
-        if (navio1_x + i < TAMANHO) {
-            tabuleiro[navio1_y][navio1_x + i] = 1;
-        }
-    }
+    printf("BATALHA NAVAL\n");
     
-    // Posicionar navio 2 (vertical)
-    for (int i = 0; i < tamanho_navio2; i++) {
-        if (navio2_y + i < TAMANHO) {
-            tabuleiro[navio2_y + i][navio2_x] = 1;
-        }
-    }
+    // Imprime o tabuleiro
+    printf("   A B C D E F G H I J\n");
     
-    // Imprimir tabuleiro
-    printf("Tabuleiro de Batalha Naval 10x10:\n\n");
-    printf("   ");
-    for (int x = 0; x < TAMANHO; x++) {
-        printf("%2d ", x);
-    }
-    printf("\n");
-    
-    for (int y = 0; y < TAMANHO; y++) {
-        printf("%2d ", y);
-        for (int x = 0; x < TAMANHO; x++) {
-            if (tabuleiro[y][x] == 1) {
-                printf(" 3 "); // 3 representa o navio
-            } else {
-                printf(" 0 "); // 0 representa água
-            }
+    for(int i = 0; i < 10; i++) {
+        printf("%2d ", i+1);
+        
+        for(int j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
